699b65f6e9d611b33ede02e44f6593d28fb86d0e
Leah Williams <leah@leahs-air.lan>
2023-11-02 11:35:38 -0400

    initial commit

---
+++ schemas/navigation.ts
@@ -0,0 +1,14 @@
+import { defineField } from "sanity";
+import {TbLayoutNavbar} from "react-icons/tb";
+
+const navigation = {
+    name: "navigation",
+    title: "navigation",
+    type: "array",
+    icon: TbLayoutNavbar ,
+    fields: [
+
+    ],
+};
+
+export default navigation;
---
+++ schemas/navigationItem.ts
@@ -0,0 +1,14 @@
+import { defineField } from "sanity";
+import {TbLayoutNavbarExpand} from "react-icons/tb";
+
+const navigationItem = {
+    name: "navigationItem",
+    title: "navigationItem",
+    type: "array",
+    icon: TbLayoutNavbarExpand ,
+    fields: [
+
+    ],
+};
+
+export default navigationItem;
